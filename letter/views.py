f81c385fb86cb3bcbe9927cea37b8dd8cc5a3a1e
seungmin Ryu <ojysep9987@naver.com>
2022-11-14 04:18:38 +0900

    Feat: Letter 관련 url 초기세팅

--- letter/views.py
+++ letter/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def test():
+    return HttpResponse(status=200)
