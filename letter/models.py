a475f96eadaf6a6cdda4ea64774139874db49b19
seungmin Ryu <ojysep9987@naver.com>
2022-11-14 03:36:11 +0900

    Feat: 편지쓰기 관련

---
+++ letter/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+# Create your models here.
+class Letter(models.Model):
+    dear = models.CharField(max_length=50)
+    writer = models.ForeignKey("accounts.User",
+                               on_delete=models.CASCADE,
+                               db_column='writer')
+    content = models.CharField(null=True)
+    is_answer = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.pk
+
+    class Meta:
+        db_table = 'letter'
+
+
+class Answer(models.Model):
+    letter = models.ForeignKey("letter.Letter",
+                               on_delete=models.CASCADE,
+                               db_column='letter')
+    responser = models.ForeignKey("accounts.User",
+                                  on_delete=models.CASCADE,
+                                  db_column='responser')
+
+    def __str__(self):
+        return self.pk
+
+    class Meta:
+        db_table = 'answer'
