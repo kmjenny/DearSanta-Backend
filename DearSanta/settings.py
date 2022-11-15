d82f9ce407a6c341e726c5c4811377f10576f4d4
kmjenny <kjn3008@gmail.com>
2022-11-15 15:19:24 +0900

    Setting

--- DearSanta/settings.py
+++ DearSanta/settings.py
@@ -110,27 +110,27 @@ WSGI_APPLICATION = 'DearSanta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#           "ENGINE": "django.db.backends.mysql",
-#           "NAME": "dearsanta",
-#           "USER": "root",
-#           "PASSWORD": "ojysep9987",
-#           "HOST": "127.0.0.1",
-#           "PORT": "3306"
-#     }
-# }
-
 DATABASES = {
     "default": {
           "ENGINE": "django.db.backends.mysql",
           "NAME": "dearsanta",
-          "USER": "admin",
-          "PASSWORD": "99879987",
-          "HOST": "database.cg8oybxtdlm3.ap-northeast-2.rds.amazonaws.com",
-          "PORT": "3307"
+          "USER": "root",
+          "PASSWORD": "jenny3532",
+          "HOST": "127.0.0.1",
+          "PORT": "3306"
     }
 }
+
+# DATABASES = {
+#     "default": {
+#           "ENGINE": "django.db.backends.mysql",
+#           "NAME": "dearsanta",
+#           "USER": "admin",
+#           "PASSWORD": "99879987",
+#           "HOST": "database.cg8oybxtdlm3.ap-northeast-2.rds.amazonaws.com",
+#           "PORT": "3307"
+#     }
+# }
 
 
 # Password validation
